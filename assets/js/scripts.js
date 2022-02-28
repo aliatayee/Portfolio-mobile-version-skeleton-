c4f96c15107e3c4342c789070a2a7877f5454d4d
Ali Aqa Atayee <74867463+aliatayee@users.noreply.github.com>
2022-02-28 19:45:31 +0430

    Revert "Mobile menu - #4th Milestone"

--- assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function menuFunction() {
-    var links = document.getElementById("mobile-menu-container");
-    if(links.style.display === "block"){
-        links.style.display ="none";
-    }else{
-        links.style.display ="block";
-    }
-}
